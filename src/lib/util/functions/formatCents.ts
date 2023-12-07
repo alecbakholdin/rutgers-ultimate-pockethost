961c5c5f22df04dd798e36c05a12c5a41329fbec
alecbakholdin <alecbakholdin@gmail.com>
2023-12-07 10:08:33 -0500

    Trigger rebuild

--- src/lib/util/functions/formatCents.ts
+++ src/lib/util/functions/formatCents.ts
@@ -1,7 +1,12 @@
-export function formatCents(num: number, options?: {excludeDollar?: boolean}) {
-  return `${options?.excludeDollar ? '' : '$'}${(num / 100).toLocaleString(undefined, {
-    minimumFractionDigits: 2,
-    maximumFractionDigits: 2,
-  })}`
+export function formatCents(
+  num: number,
+  options?: { excludeDollar?: boolean },
+) {
+  return `${options?.excludeDollar ? '' : '$'}${(num / 100).toLocaleString(
+    undefined,
+    {
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2,
+    },
+  )}`
 }
-
