3254032d79cf9d045784dc6391185a622ac870a1
alecbakholdin <alecbakholdin@gmail.com>
2023-12-07 11:32:14 -0500

    Navbar Fixes

--- src/lib/component/Navbar.svelte
+++ src/lib/component/Navbar.svelte
@@ -4,53 +4,53 @@
   import Icon from '@iconify/svelte'
 </script>
 
-<div class="bg-base-200">
-  <div class="max-w-5xl mx-auto navbar">
-    <a href="/" class="navbar-start btn btn-ghost">
-      <!-- <img src="/machine.png" width={50} height={50} alt="machine"/>
-      <img src="/nightshade.png" width={50} height={50} alt="nightshade"/> -->
-      <img src="/machine-nightshade.png" class="h-[25px] transition-all sm:-rotate-90" alt="machine-nightshade"/>
-      <span class="text-xl invisible sm:visible">Rutgers Ultimate</span>
-    </a>
-    <div class="navbar-end">
-      <ul class="menu menu-horizontal">
-        {#if $currentUser}
-          <li>
-            <a href="/cart">
-              <Icon icon="mdi:cart" class="text-base-content text-xl" />
-            </a>
-          </li>
-          <li>
-            <details>
-              <summary><Icon icon="mdi:menu"/></summary>
-              <ul class="p-2 bg-base-100 rounded-t-none">
-                  <li>
-                    <a href="/orders">
-                      My Orders
-                    </a>
-                  </li>
-                  <li>
-                    <form
-                      method="POST"
-                      action="/logout"
-                      use:enhance={() => {
-                        return async ({ result }) => {
-                          pb.authStore.clear()
-                          await applyAction(result)
-                        }
-                      }}
-                    >
-                      <button>Log out</button>
-                    </form>
-                  </li>
-                </ul>
-            </details>
-          </li>
-        {:else}
-          <li><a href="/login">Log in</a></li>
-          <li><a href="/register">Register</a></li>
-        {/if}
-      </ul>
-    </div>
+<div class="navbar bg-base-200">
+  <div class="flex-1">
+    <a href="/" class="btn btn-ghost">
+      <img
+        src="/machine-nightshade.png"
+        class="h-[25px] transition-all sm:-rotate-90"
+        alt="machine-nightshade"
+      />
+      <span class="text-xl invisible sm:visible">Rutgers Ultimate</span></a
+    >
+  </div>
+  <div class="flex-none">
+    <ul class="menu menu-horizontal px-1">
+      {#if $currentUser}
+        <li>
+          <a href="/cart">
+            <Icon icon="mdi:cart" class="text-base-content text-xl" />
+          </a>
+        </li>
+        <li>
+          <details>
+            <summary> More </summary>
+            <ul class="p-2 bg-base-100 rounded-t-none">
+              <li>
+                <a href="/orders"> Orders </a>
+              </li>
+              <li>
+                <form
+                  method="POST"
+                  action="/logout"
+                  use:enhance={() => {
+                    return async ({ result }) => {
+                      pb.authStore.clear()
+                      await applyAction(result)
+                    }
+                  }}
+                >
+                  <button>Logout</button>
+                </form>
+              </li>
+            </ul>
+          </details>
+        </li>
+      {:else}
+        <li><a href="/login">Log in</a></li>
+        <li><a href="/register">Register</a></li>
+      {/if}
+    </ul>
   </div>
 </div>
