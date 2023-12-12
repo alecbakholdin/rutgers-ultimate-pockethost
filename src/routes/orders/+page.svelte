be30be224aa50001d4c9b1bb592d8d622c9921d3
alecbakholdin <alecbakholdin@gmail.com>
2023-12-12 02:31:51 -0500

    Fixed the order page with suggestions from a very talented artist

--- src/routes/orders/+page.svelte
+++ src/routes/orders/+page.svelte
@@ -14,15 +14,14 @@
 <div class="max-w-lg m-auto flex flex-col gap-2">
   {#each data.orders.items as order, i (order.id)}
     {@const createdDate = new Date(order.created)}
-    {@const fulfilledDate = order.fulfilled
-      ? new Date(order.fulfilled).toDateString()
-      : 'Unfulfilled'}
+    {@const fulfillIcon = order.fulfilled ? 'mdi:done' : 'mdi:remove'}
+    {@const fulfilledDate = order.fulfilled ? 'Fulfilled' : 'Unfulfilled'}
     {@const totalFormatted = formatCents(order.total, { excludeDollar: true })}
     {@const lineItems = order.expand?.['order_line_item(order)'] || []}
     {@const numItems = _.sumBy(lineItems, 'quantity')}
     <button
       type="button"
-      class="w-full bg-base-100 border rounded-xl p-4 grid grid-cols-[auto_1fr] gap-2"
+      class="w-full bg-base-100 border rounded-xl p-4 grid grid-cols-[auto_1fr] gap-x-2 gap-y-4"
       on:click={() => (expandedIdx = expandedIdx !== i ? i : undefined)}
     >
       <div
@@ -41,17 +40,15 @@
           <p class="m-0 p-0">{createdDate.getDate()}</p>
           <p class="text-xs m-[-3px]">{shortMonths[createdDate.getMonth()]}</p>
         </div>
-        <!-- <div
-          class="border w-[40px] h-[40px] rounded-md flex flex-col items-center text-sm font-semibold"
-        >
-          <p>{lineItems.length}</p>
-          <p class="text-xs">Items</p>
-        </div> -->
+        <p class="font-semibold flex-grow text-end">{formatCents(order.total)}</p>
+        <!-- <button type="button" class="btn btn-ghost btn-xs" on:click|stopPropagation={() => {}}>
+          <Icon icon="ph:dots-three-vertical-bold" class="flex-shrink text-lg"/>
+        </button> -->
       </div>
       <div />
-      <div class="text-sm text-left flex gap-2">
+      <div class="text-sm text-left flex gap-3">
         <div>
-          {#each [['mdi:dollar', totalFormatted], ['mdi:done', fulfilledDate], ['ant-design:number-outlined', `${numItems} items`]] as [icon, text]}
+          {#each [[fulfillIcon, fulfilledDate], ['ant-design:number-outlined', `${numItems} items`]] as [icon, text]}
             <div class="flex items-center w-fit gap-1">
               <Icon {icon} />
               <p>{text}</p>
@@ -64,13 +61,12 @@
         </div>
         {#if order.shippingAddress && order.shippingCostInCents}
           {@const addr = order.shippingAddress}
-          <div class="flex items-start gap-1 w-fit border p-1 rounded-md">
-            <Icon icon="material-symbols:local-shipping" class="text-3xl p-1" />
+          <div class="flex gap-1 items-start">
+            <Icon icon="mdi:location" class="pt-1 text-lg flex-shrink-0" />
             <div>
               <p>{addr.line1}</p>
               <p>{addr.line2}</p>
               <p>{addr.city}, {addr.state} {addr.postal_code}</p>
-              <p>{formatCents(order.shippingCostInCents)}</p>
             </div>
           </div>
         {/if}
@@ -78,6 +74,7 @@
 
       {#if expandedIdx === i}
         <div class="flex flex-col col-span-full gap-2" transition:slide>
+          <div class="divider h-[1px] mb-4 mt-0"></div>
           {#each lineItems as lineItem (lineItem.id)}
             {@const product = lineItem.expand?.product}
             {#if product}
@@ -87,22 +84,36 @@
                   alt={product.title}
                   image={product.primaryImage}
                   size={100}
-                  class="rounded-sm"
+                  class="rounded-sm flex-shrink-0"
                 />
-                <div class="flex-grow text-left">
+                <div
+                  class="grid grid-cols-[1fr_auto] place-items-start h-fit w-full"
+                >
                   <p class="font-semibold">
                     {lineItem.quantity} x {product.title}
                   </p>
+                  <p class="font-semibold text-sm place-self-end">
+                    {formatCents(lineItem.totalCents)}
+                  </p>
                   {#each Object.entries(lineItem.fields || {}) as [key, value]}
-                    <p>{key}: {value}</p>
+                    <p class="col-span-full">{key}: {value}</p>
                   {/each}
                 </div>
-                <p class="font-semibold text-sm self-center">
-                  {formatCents(lineItem.totalCents)}
-                </p>
               </div>
             {/if}
           {/each}
+          {#if order.shippingCostInCents}
+            <div class="divider h-[1px] mt-4"></div>
+            <div class="flex gap-2 w-full items-center mb-2">
+              <div class="w-[100px] flex-shrink-0 flex justify-center">
+                <Icon icon="material-symbols:local-shipping" class="text-3xl" />
+              </div>
+              <p class="font-semibold flex-grow text-start">Shipping</p>
+              <p class="font-semibold text-sm">
+                {formatCents(order.shippingCostInCents)}
+              </p>
+            </div>
+          {/if}
         </div>
       {/if}
     </button>
