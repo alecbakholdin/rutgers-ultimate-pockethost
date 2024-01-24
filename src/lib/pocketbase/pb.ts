564c3d0dc374541f18fc95081897d82aee1a75e7
Alec Bakholdin <43560338+alecbakholdin@users.noreply.github.com>
2024-01-24 14:51:32 +0000

    disabled auto cancellation globally

--- src/lib/pocketbase/pb.ts
+++ src/lib/pocketbase/pb.ts
@@ -3,7 +3,7 @@ import PocketBase from 'pocketbase'
 import { writable } from 'svelte/store'
 import type { UsersResponse, TypedPocketBase } from './pocketbase-types'
 
-export const pb = new PocketBase(PUBLIC_POCKETBASE_URL) as TypedPocketBase
+export const pb = new PocketBase(PUBLIC_POCKETBASE_URL).autoCancellation(false) as TypedPocketBase
 
 export const currentUser = writable<UsersResponse | undefined>(
   pb.authStore.model as any,
