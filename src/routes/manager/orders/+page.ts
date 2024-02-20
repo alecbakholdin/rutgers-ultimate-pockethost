e3be061159552bc7d64b1b56dcf8fe932c2291f9
alecbakholdin <alecbakholdin@gmail.com>
2024-02-20 15:19:09 -0500

    can now search number using f.Number

--- src/routes/manager/orders/+page.ts
+++ src/routes/manager/orders/+page.ts
@@ -5,26 +5,34 @@ import {
 import { pb } from '$lib/pocketbase/pb.js'
 
 export async function load({ url }) {
-  const search = url.searchParams.get('q')
+  const search = url.searchParams.get('q') || ''
 
-  if (!search) {
-    return {
-      orders: [],
-    }
-  }
-
+  const r = /^(?:fields)|f\.([\w\s]+)=(.*)$/
   const searchTerms = search.split(/\s+/g)
-  const filterStr = searchTerms
-    .map((_, i) => `(${
-      ['order.user.name', 'order.user.email', 'product.title', 'fields'].map(
-        (field) => `${field} ~ {:q${i}}`,
-      ).join(' || ')})`,
+  const stringSearchClauses = searchTerms
+    .filter((s) => !s.match(r))
+    .map(
+      (_, i) =>
+        `(${['order.user.name', 'order.user.email', 'product.title', 'fields']
+          .map((field) => `${field} ~ {:q${i}}`)
+          .join(' || ')})`,
     )
-    .join(' && ')
-  const filterObj = searchTerms.reduce(
+  const stringSearchObj = stringSearchClauses.reduce(
     (acc, _, i) => ({ ...acc, [`q${i}`]: searchTerms[i] }),
     {},
   )
+
+  const fieldFilters = searchTerms.map((s) => s.match(r)).filter(Boolean)
+  const fieldFilterClauses = fieldFilters.map(
+    (f, i) => `fields.${f![1]} = {:f${i}}`,
+  )
+  const fieldFilterObj = fieldFilters.reduce(
+    (acc, f, i) => ({ ...acc, [`f${i}`]: f![2] }),
+    {},
+  )
+  const filterStr = [...fieldFilterClauses, ...stringSearchClauses].join(' && ')
+  const filterObj = { ...fieldFilterObj, ...stringSearchObj };
+
 
   const filter = pb.filter(filterStr, filterObj)
   console.log(filter)
