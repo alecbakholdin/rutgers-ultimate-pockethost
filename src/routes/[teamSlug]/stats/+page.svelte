da98c6bd00c52ccc2351788f7b8d705e86446fea
Alec Bakholdin <alecbakholdin@gmail.com>
2024-03-02 06:30:24 -0500

    Statistics page

---
+++ src/routes/[teamSlug]/stats/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  export let data;
+   //$: players = data.players?.map(x => x.)
+   
+</script>
+<div class="overflow-x-auto mt-2">
+    <table class="table">
+        <thead>
+
+        </thead>
+    </table>
+</div>
