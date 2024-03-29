196690959a8fb84e0ace77f43048c4c3b763989c
alecbakholdin <alecbakholdin@gmail.com>
2024-03-29 13:41:24 -0400

    moved everything into layout.sever.ts

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,3 +1,20 @@
-export async function load({ locals: { user } }) {
-  return { user }
+import type {
+  TeamResponse,
+  GameResponse,
+} from '$lib/pocketbase/pocketbase-types'
+
+export async function load({ url, locals: { user, pb } }) {
+  const liveTeams = await pb
+    .collection('team')
+    .getFullList<TeamResponse<{ live_game: GameResponse }>>({
+      filter: 'live_game != null',
+      expand: 'live_game',
+    })
+
+  return {
+    liveTeams: liveTeams.filter(
+      (team) => `/${team.slug}/live` !== url.pathname,
+    ),
+    user
+  }
 }
