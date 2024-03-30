19de6e9b52da7b4c2cb45c9e39893c9fed37bf68
Alec Bakholdin <43560338+alecbakholdin@users.noreply.github.com>
2024-03-30 18:11:05 +0000

    changes date format

--- src/lib/util/functions/formatDate.ts
+++ src/lib/util/functions/formatDate.ts
@@ -18,7 +18,7 @@ export function formatDate(date: Date | string | undefined | null) {
   date = typeof date === 'string' ? new Date(date) : date
   const month = months[date.getMonth()]
   const day = date.getDate()
-  const hour = date.getHours() % 13
+  const hour = date.getHours() === 12 ? 12 : (date.getHours() % 12)
   const minute = (date.getMinutes() < 10 ? '0' : '') + date.getMinutes()
   const ampm = date.getHours() >= 12 ? 'PM' : 'AM'
   return `${month} ${day} @ ${hour}:${minute} ${ampm}`
