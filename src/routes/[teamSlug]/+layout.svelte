900694cbd26cea991335e4d240e200cca6ffe304
Alec Bakholdin <abakho@icims.com>
2024-10-16 19:28:29 -0400

    Updated pocketbase types and added archived indicator when team is archived

--- src/routes/[teamSlug]/+layout.svelte
+++ src/routes/[teamSlug]/+layout.svelte
@@ -6,9 +6,21 @@
   $: params = Object.entries($page.params ?? {}) as [string, string][]
   $: getCrumbText = (part: string) =>
     params.reduce((a, [key, val]) => a.replace(`[${key}]`, val), part)
+
+  export let data
 </script>
 
 <div>
+  {#if data.team?.archived}
+    <div role="alert" class="alert">
+      <Icon icon="ri:archive-line" class="text-xl" />
+      <span>
+        This team has been archived and is no longer active, but stats are still
+        viewable. Navigate to the <a href="/" class="underline"> home page </a>
+        to see the currently active team(s).
+      </span>
+    </div>
+  {/if}
   <div class="text-sm breadcrumbs">
     <ul>
       <li><a href="/"><Icon icon="mdi:home" /></a></li>
